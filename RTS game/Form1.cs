641ab26fdb4c8c3d737eabd7258a051abb92d157
tigranohannessian <tigranohannessian@gmail.com>
2019-10-07 15:52:29 +0200

    first commit

---
+++ RTS game/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace RTS_game
+{
+    public partial class Form1 : Form
+    {
+
+        private int timertracker;
+       
+        Map mp;
+        
+ 
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        
+
+      public void lblMAP_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblRes.Visible = true;
+            
+
+        }
+    //creating method for display
+       
+
+        private void curROUND_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mapgrp_Enter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private  void btnSTART_Click(object sender, EventArgs e)
+        {
+            
+           // Update(Mapgrp);
+
+            
+            
+            Map map = new Map(20, infotxt);
+            map.Generate();
+            map.Display(Mapgrp);
+           // map.displayBuildings(Mapgrp);
+            
+            
+
+          
+
+            
+            
+
+           
+
+            
+
+
+
+
+        }
+
+        private void btnPAUSE_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+        //int seconds = 0;
+       // int minutes = 0;
+       // int count;
+       
+
+
+        // trying to stop timer at a certian number so rounds can stop and as a result resorce genaration stops
+        // BUT IT JUST WONT
+       
+      
+
+       
+
+        private void infotxt_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblResorces_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void resourcegrp_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        // updates text for resources lable when clicked
+       
+
+        private void lblROUND_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTNREAD_Click(object sender, EventArgs e)
+        {
+            
+            mp.readmap();
+        }
+    }
+                
+    }
+    
+
+
+
---
+++ RTS game/Map.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace RTS_game
+{
+    [Serializable]
+    public class Map
+    {
+
+
+        //Create constants in the “Map class”   (research what a constant is)
+
+        List<UNIT> units = new List<UNIT>();
+        
+        Random r = new Random();
+        int numUnits = 0;
+        TextBox Infotxt;
+
+        public List<UNIT> Units
+        {
+            get { return units; }
+            set { units = value; }
+        }
+
+       
+
+        public Map(int n, TextBox txt)
+        {
+            numUnits = n;
+            Infotxt = txt;
+        }
+
+
+      
+
+
+        
+
+        public void Generate()
+        {
+            for (int i = 0; i < numUnits; i++)
+            {
+                if (r.Next(0, 2) == 0) //Generate MeleeUnit
+                {
+                    MeleeUnit m = new MeleeUnit(r.Next(0, 20),
+                                                r.Next(0, 20),
+                                                100,
+                                                50,
+                                                10,
+                                                10,
+
+                                                (i % 2 == 0 ? 1 : 0),
+                                                "M",
+                                                  ///unit name added to constructers Uname
+                                                  "knight");
+                    units.Add(m);
+                }
+                else //Generate RangedUnit
+                {
+                    RangedUnit ru = new RangedUnit(r.Next(0, 20),
+                                                   r.Next(0, 20),
+                                                   100,
+                                                   1,
+                                                   20,
+                                                   5,
+                                                   (i % 2 == 0 ? 1 : 0),
+                                                   "R",
+                                                    ///unit name added to constructers Uname
+                                                    "archer");
+                    units.Add(ru);
+
+                }
+               
+                
+
+
+
+
+            }
+
+        }
+
+
+
+        
+
+
+
+        public void Display(GroupBox groupBox)
+        {
+            groupBox.Controls.Clear();
+            foreach (UNIT u in units)
+            {
+                Button b = new Button();
+                if (u is MeleeUnit)
+                {
+                    MeleeUnit mu = (MeleeUnit)u;
+                    mu.Symbol = "M";
+                    b.Size = new Size(20, 20);
+                    b.Location = new Point(mu.Xpos * 20, mu.Ypos * 20);
+                    b.Text = mu.Symbol;
+
+
+                    if (mu.Faction == 0)
+                    {
+                        b.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        b.ForeColor = Color.Green;
+                    }
+
+                }
+                else
+                {
+                    RangedUnit ru = (RangedUnit)u;
+                    ru.Symbol = "R.";
+                    b.Size = new Size(20, 20);
+                    b.Location = new Point(ru.Xpos * 20, ru.Ypos * 20);
+                    b.Text = ru.Symbol;
+
+                    if (ru.Faction == 0)
+                    {
+                        b.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        b.ForeColor = Color.Green;
+                    }
+
+
+
+                    {
+
+                        b.Click += Unit_Click;
+                        groupBox.Controls.Add(b);
+
+
+
+
+
+
+                    }
+                }
+
+            }
+        }
+        public void Unit_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            Button b = (Button)sender;
+            x = b.Location.X / 20;
+            y = b.Location.Y / 20;
+            foreach (UNIT u in units)
+            {
+                if (u is RangedUnit)
+                {
+                    RangedUnit ru = (RangedUnit)u;    // ranged unit
+                    if (ru.Xpos == x && ru.Ypos == y)
+                    {
+                        Infotxt.Text = "R";
+                        Infotxt.Text = ru.ToString();
+                    }
+                    else if (u is MeleeUnit)
+                    {
+                        MeleeUnit mu = (MeleeUnit)u;      //melee unit
+                        if (mu.Xpos == x && mu.Ypos == y)
+                        {
+                            Infotxt.Text = "M";
+                            Infotxt.Text = mu.ToString();
+                        }
+
+                    }
+
+                   
+
+
+                }
+            }
+        }
+    }
+}
+
+        
+    
+
+            
+        
+
+
+
+
+
+
+
+
+
